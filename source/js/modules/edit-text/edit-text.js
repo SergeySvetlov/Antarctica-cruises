0adf93366ab193e15682c28ae1b8e5d30ccafefb
SergeySvetlov <sergey.svetlov.91@list.ru>
2023-01-13 11:31:45 +0300

    исправил картинки, убрал маркер

--- source/js/modules/edit-text/edit-text.js
+++ source/js/modules/edit-text/edit-text.js
@@ -12,6 +12,9 @@ const paragraphForm = document.querySelector('[data-paragraph-form]');
 const editText = () => {
 
   if (windowSize <= '1022') {
+    if (paragraphAbout !== null) {
+      paragraphAbout.innerHTML = 'Это волшебное путешествие поможет вам познакомиться поближе<br> с удивительным миром Антарктики. Наш круизный лайнер отвезет вас<br> на край Земли, где вы сможете увидеть: ледяные айсберги,<br> несколько видов пингвинов, китов, морских котиков и многое другое.';
+    }
     if (headerAllInclusive !== null) {
       headerAllInclusive.innerHTML = 'Система<br> все включено';
     }
@@ -28,6 +31,9 @@ const editText = () => {
       paragraphAddress.innerHTML = 'г. Санкт Петербург,<br> ул. Большая Конюшенная, 19/8';
     }
   } else {
+    if (paragraphAbout !== null) {
+      paragraphAbout.innerHTML = 'Это волшебное путешествие поможет вам познакомиться поближе с удивительным миром Антарктики. Наш круизный лайнер отвезет вас на край Земли, где вы сможете увидеть: ледяные айсберги, несколько видов пингвинов, китов, морских котиков и многое другое.';
+    }
     if (headerAllInclusive !== null) {
       headerAllInclusive.innerHTML = 'Система все включено';
     }
@@ -65,28 +71,6 @@ const editTextMobile = () => {
     }
     if (paragraphForm !== null) {
       paragraphForm.innerHTML = 'Оставьте заявку<br> и мы свяжемся с вами<br> в ближайшее время';
-    }
-    if (paragraphDescription !== null) {
-      paragraphDescription.innerHTML = 'У вас ещё остались вопросы или сомнения? Приезжайте к нам в офис и мы вам все расскажем.';
-    }
-  } else {
-    if (headerBottom !== null) {
-      headerBottom.innerHTML = 'Круизы в Антарктику';
-    }
-    if (paragraphAbout !== null) {
-      paragraphAbout.innerHTML = 'Это волшебное путешествие поможет вам познакомиться поближе<br> с удивительным миром Антарктики. Наш круизный лайнер отвезет вас<br> на край Земли, где вы сможете увидеть: ледяные айсберги,<br> несколько видов пингвинов, китов, морских котиков и многое другое.';
-    }
-    if (paragraphLiner !== null) {
-      paragraphLiner.innerHTML = 'Круизный лайнер, рассчитанный всего лишь на 300 гостей, поможет избежать шума и очередей и даст возможность насладиться видами и природой.';
-    }
-    if (paragraphAllInclusive !== null) {
-      paragraphAllInclusive.innerHTML = 'Все наши круизы работают по системе “Все Включено”. Великолепные блюда и безлимитный бар доступны для вас 24 часа.';
-    }
-    if (feedbackText !== null) {
-      feedbackText.innerHTML = 'Забронируйте тур за 3-6 месяцев<br> до даты выезда и сэкономьте до 50%';
-    }
-    if (paragraphForm !== null) {
-      paragraphForm.innerHTML = 'Оставьте заявку и мы свяжемся с вами в ближайшее время';
     }
     if (paragraphDescription !== null) {
       paragraphDescription.innerHTML = 'У вас ещё остались вопросы или сомнения? Приезжайте к нам в офис и мы вам все расскажем.';
